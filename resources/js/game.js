034230435a71a17cd5e2c85f9ce1bdb4d7803e83
Ian Travers <mr.itpe@gmail.com>
2022-03-29 16:16:56 +0300

    Submit a Guess [E06] (use a getter)

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,9 +1,14 @@
 import Tile from "./Tile";
 
 export default {
+    theWord: 'cat',
     guessesAllowed: 4,
     wordLength: 3,
     currentRowIndex: 0,
+
+    get currentGuess() {
+        return this.currentRow.map(tile => tile.letter).join('')
+    },
 
     init() {
         this.board = Array.from({ length: this.guessesAllowed }, () => {
@@ -12,14 +17,15 @@ export default {
     },
 
     onKeyPress(key) {
-        // validate: only letter
         if (/^[A-z]$/.test(key)) {
             this.fillTile(key)
+        } else if (key === 'Enter') {
+            this.submitGuess()
         }
     },
 
     fillTile(key) {
-        for (let tile of this.currentRow()) {
+        for (let tile of this.currentRow) {
             if (!tile.letter) {
                 tile.fill(key)
 
@@ -28,7 +34,23 @@ export default {
         }
     },
 
-    currentRow() {
+    submitGuess() {
+        let guess = this.currentGuess
+
+        if (guess.length < this.wordLength) {
+            return
+        }
+
+        if (guess === this.theWord) {
+            alert('You win!')
+        } else {
+            alert('Nope')
+
+            this.currentRowIndex++
+        }
+    },
+
+    get currentRow() {
         return this.board[this.currentRowIndex]
     }
 }
