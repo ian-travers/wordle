5b6529f43f3e52438f43eaf90d0e7e7bc7a7adfd
Ian Travers <mr.itpe@gmail.com>
2022-03-29 15:51:36 +0300

    The Source of Truth [E05] (Tile class 15:55)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,26 @@
+class Tile {
+    letter = ''
+    status = '' // correct, present, absent
+
+    fill(key) {
+        this.letter = key.toLowerCase()
+    }
+
+    empty() {
+        this.letter = ''
+    }
+}
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('game', () => {
         return {
             guessesAllowed: 4,
             wordLength: 3,
             currentRowIndex: 0,
-            currentTileIndex: 0,
 
             init() {
                 this.board = Array.from({ length: this.guessesAllowed }, () => {
-                    return Array.from({ length: this.wordLength }, () => '')
+                    return Array.from({ length: this.wordLength }, () => new Tile)
                 })
             },
 
@@ -20,14 +32,17 @@ document.addEventListener('alpine:init', () => {
             },
 
             fillTile(key) {
-                this.board[this.currentRowIndex][this.currentTileIndex] = key
+                for (let tile of this.currentRow()) {
+                    if (!tile.letter) {
+                        tile.fill(key)
 
-                if (this.currentTileIndex === this.wordLength - 1) {
-                    this.currentRowIndex++
-                    this.currentTileIndex = 0
-                } else {
-                    this.currentTileIndex++
+                        break;
+                    }
                 }
+            },
+
+            currentRow() {
+                return this.board[this.currentRowIndex]
             }
         }
     })
