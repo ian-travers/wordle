da94647a7849028c46ca8d9449015871ffacaede
Ian Travers <mr.itpe@gmail.com>
2022-03-29 16:00:12 +0300

    The Source of Truth [E05] (Extract Tile class and game object as separate files 16:00)

---
+++ resources/js/Tile.js
@@ -0,0 +1,12 @@
+export default class Tile {
+    letter = ''
+    status = '' // correct, present, absent
+
+    fill(key) {
+        this.letter = key.toLowerCase()
+    }
+
+    empty() {
+        this.letter = ''
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,49 +1,5 @@
-class Tile {
-    letter = ''
-    status = '' // correct, present, absent
-
-    fill(key) {
-        this.letter = key.toLowerCase()
-    }
-
-    empty() {
-        this.letter = ''
-    }
-}
+import game from "./game";
 
 document.addEventListener('alpine:init', () => {
-    Alpine.data('game', () => {
-        return {
-            guessesAllowed: 4,
-            wordLength: 3,
-            currentRowIndex: 0,
-
-            init() {
-                this.board = Array.from({ length: this.guessesAllowed }, () => {
-                    return Array.from({ length: this.wordLength }, () => new Tile)
-                })
-            },
-
-            onKeyPress(key) {
-                // validate: only letter
-                if (/^[A-z]$/.test(key)) {
-                    this.fillTile(key)
-                }
-            },
-
-            fillTile(key) {
-                for (let tile of this.currentRow()) {
-                    if (!tile.letter) {
-                        tile.fill(key)
-
-                        break;
-                    }
-                }
-            },
-
-            currentRow() {
-                return this.board[this.currentRowIndex]
-            }
-        }
-    })
+    Alpine.data('game', () => game)
 })
