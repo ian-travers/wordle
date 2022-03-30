165691298058dddc7dce526aee6302d11827ead2
Ian Travers <mr.itpe@gmail.com>
2022-03-30 14:47:11 +0300

    Make it Look Professional [E10]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,9 +15,12 @@
     x-data="game"
     @keyup.window="onKeyPress($event.key)"
 >
+    <h1 aria-label="TryCat">
+        <img src="/images/trycat-logo.svg" alt="logo">
+    </h1>
     <div id="game">
-        <template x-for="row in board">
-            <div class="row">
+        <template x-for="(row, index) in board">
+            <div class="row" :class="{'current': currentRowIndex === index}">
                 <template x-for="tile in row">
                     <div class="tile" :class="tile.status" x-text="tile.letter"></div>
                 </template>
